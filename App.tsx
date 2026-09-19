49585574cf79cd81e308635627a773951f25833c
Wendy Arango <warangochavarria@recurly.com>
2026-09-19 13:33:51 -0500

    commit inicial

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Cronos App</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
